cfbb8b1d5c6acf0cb9537dded547334f86c69f13
Yevhen Podoliaka <zanp85740@gmail.com>
2022-07-19 10:53:02 +0300

    bugfix

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -3,7 +3,6 @@ import s from './FeedbackOptions.module.css';
 const FeedbackOptions = ({ options, onLeaveFeedback }) => {
   return (
     <div className={s.wrap}>
-      {' '}
       {options.map(key => (
         <button key={key} type="button" name={key} onClick={onLeaveFeedback}>
           {key}
